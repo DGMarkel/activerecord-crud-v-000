181b14cb7683e540a9686607b350e2570a03c154
David Markel <davidgmarkel@gmail.com>
2018-05-02 15:49:02 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -64,7 +64,7 @@ end
 def can_be_found_updated_and_saved
   # Updtate the title "Awesome Flick" to "Even Awesomer Flick"
   movie = Movie.create(title: "Awesome Flick")
-  movie.update(1, :title => "Even Awesomer Flick")
+  movie.where("id = 1").update(:title => "Even Awesomer Flick")
 end
 
 def can_update_using_update_method
